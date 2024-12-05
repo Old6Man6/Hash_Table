b1e28a9a3d6a422700ed72561e4192e59c90aa6d
sadeq <sadeghtpf13@gmail.com>
2024-12-06 00:31:42 +0330

    2
    Create Node class for making a linked list to store key-value pair
    
    and making this Node helps to handle collision problem

--- hash_table.py
+++ hash_table.py
@@ -1,10 +1,18 @@
 import hashlib
 
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
 class HashTable:
     def __init__(self, size=8):
         self.table = [[None] for _ in range(size)]
 
-    '''calculate key hash and where index '''
+    '''Hash the key and find the index'''
     def hash(self, key):
         hash_hex = hashlib.sha256(key.encode()).hexdigest() # get hexdecimal
         index = int(hash_hex, 16) # convert to integer
@@ -13,7 +21,9 @@ class HashTable:
 
         '''if key dosen't exist create it if exist and value are diffrent update value'''
     def create_update(self, key, value):
-        pass
+        index = self.hash(key)
+        if self.table[index] is None:
+
 
 
     def delete(self, key):
