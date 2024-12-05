d9e1317ea68deeb8f8f3060d78cf9a3cc036a184
sadeq <sadeghtpf13@gmail.com>
2024-12-06 01:15:32 +0330

    3
    complete the (create_update) func
    
    make and change some comment
    
    *handling the collision in inserting by Separate-Chaining method

--- hash_table.py
+++ hash_table.py
@@ -19,12 +19,31 @@ class HashTable:
         return index
 
 
-        '''if key dosen't exist create it if exist and value are diffrent update value'''
+        '''Create key-value pair or Update the value if the key exist'''
     def create_update(self, key, value):
         index = self.hash(key)
-        if self.table[index] is None:
 
+        if self.table[index] is None: # If is empty
+            self.table[index] = Node(key, value) # Create new node
+            print(f"Key ---> '{key}' added with value ---> '{value}' at index ---> {index}.")
 
+        else:
+            current = self.table[index]
+            while current:
+
+                if current.key == key:  # If key is the same just update value
+                    current.value = value
+                    print(f"Key ---> {key} updated with new value ---> {value}")
+
+                if current.next is None:
+                    break # Find last node
+
+                '''Create new node in end of the list'''
+                current = current.next
+                new_Node = Node(key, value)
+                current.next = new_Node # chained new node as next node of current
+                new_Node.prev = current # chained current node as previous node
+                print(f"Collision handled: Key '{key}' added with value '{value}'.")
 
     def delete(self, key):
         pass
